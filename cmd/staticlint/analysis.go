b54fd8eb16c30e1221a820fa9f57b4c98a2bd821
Сергей Фищук <sergey.fischuk@mail.ru>
2024-09-23 17:44:52 +0300

    добавил анализатор os.Exit в main

---
+++ cmd/staticlint/analysis.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"honnef.co/go/tools/staticcheck"
+)
+
+const (
+	funcName    = "main"
+	packageName = "main"
+)
+
+var ErrNoExitAnalizer = &analysis.Analyzer{
+	Name: "ErrNoExitAnalizer",
+	Doc:  "Check for os.Exit in main function",
+	Run:  run,
+}
+//Run - функция, которая выполняет анализ кода на наличие os.Exit в функции main
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		if file.Name.Name != packageName {
+			continue
+		}
+		ast.Inspect(file, func(node ast.Node) bool {
+			switch x := node.(type) {
+			case *ast.FuncDecl: //если это функция
+				if x.Name.Name == funcName { //и имя функции равно main
+					ast.Inspect(x.Body, func(node ast.Node) bool { //итерируемся по всем операторам в теле функции
+						switch stmt := node.(type) {
+						case *ast.CallExpr: //если вызов функции
+							switch fun := stmt.Fun.(type) {
+							case *ast.SelectorExpr: //если вызов функции 
+								if fun.Sel.Name == "Exit" &&
+									fun.X.(*ast.Ident).Name == "os" { //если имя функции равно Exit и имя пакета равно os
+									pass.Reportf(stmt.Pos(), "os.Exit called in main function not allowed") //сообщение об ошибке
+								}
+							}
+						}
+						return true
+					})
+				}
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
+
+func main() {
+
+	var myChecks = []*analysis.Analyzer{
+		ErrNoExitAnalizer,
+	}
+
+	for _, v := range staticcheck.Analyzers {
+		myChecks = append(myChecks, v.Analyzer)
+	}
+	multichecker.Main(myChecks...)
+
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/FischukSergey/urlshortener.git
 
-go 1.22.0
+go 1.22.1
+
+toolchain go1.22.7
 
 require (
 	github.com/brianvoe/gofakeit/v6 v6.28.0
@@ -18,10 +20,12 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	honnef.co/go/tools v0.5.1 // indirect
 )
 
 require (
@@ -29,5 +33,6 @@ require (
 	github.com/go-chi/render v1.0.3
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240531212143-b6235391adb3
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
